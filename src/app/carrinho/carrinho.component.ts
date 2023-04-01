6f5ca9f018e5044de9aa45201d4d3647e2804c08
Jonathan Volpato de Melo <107862423+jonathanvmelo@users.noreply.github.com>
2023-04-01 15:24:11 -0300

    initial commit

---
+++ src/app/carrinho/carrinho.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
+import { IProdutoCarrinho } from '../produtos';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-carrinho',
+  templateUrl: './carrinho.component.html',
+  styleUrls: ['./carrinho.component.css']
+})
+export class CarrinhoComponent implements OnInit {
+  itensCarrinho: IProdutoCarrinho[] = [];
+  total = 0;
+  
+  constructor(
+    private router: Router,
+    public carrinhoService: CarrinhoService 
+  ){}
+
+  ngOnInit(): void {
+    this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calcularTotal(); 
+  }
+
+  calcularTotal(){
+    this.total = this.itensCarrinho.reduce((prev, curr) => (prev + curr.preco * curr.quantidade), 0); 
+  }
+
+  removerProdutoCarrinho(produtoId: number){
+    this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId); 
+    this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calcularTotal();
+  }
+
+  comprar(){
+    alert("Parabéns pela compra!");
+    this.carrinhoService.limparCarrinho();
+    this.router.navigate(["produtos"]);
+  }
+}
